f60160659c1b7087333ddc6c1728b1f5c6f5f62c
George <bluesilverg22@gmail.com>
2020-09-20 14:22:15 +0100

    Adding initial files. doesn't do anything yet

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <math.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+    float z[1760];
+    char b[1760];
+    printf("\x1b[2J");  //ANSI escape sequence, \x1b is next, [ is control sequence introducer
+                        //and 2J following the CSI clears the screen, leaving cursor where it is.
+                        //WHOLE EFFECT IS SIMPLY TO CLEAR THE SCREEN
+    
+}
