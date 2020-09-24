d59cd7eb095b13f4870fa5c6d8b4ad1bee82721a
George <bluesilverg22@gmail.com>
2020-09-24 09:15:56 +0100

    C++ drawing works. rendered circles

--- alternative.cpp
+++ alternative.cpp
@@ -1,36 +1,42 @@
 #include <math.h>
 #include <iostream>
 #include <cstdlib>
+#include <string>
 #include <unistd.h>
 using namespace std;
+
 
 int main() {
     printf("\x1b[2J");
     for (;;) {
-    char output[800];
-    for (int i = 0; i < 800; i++) {
+    char output[3200];
+    for (int i = 0; i < 3200; i++) {
         output[i] = '.';
     }
-    int corr = 0;
-    int counter = 0;
-    for (int i = 0; i < 800; i++) {
-        /*if (i % 10 == 0 && i != 0) {
-            corr = (i + counter) % 10;
-            counter++;
-            output[i] = '4';
-        }*/
-        int colno = i % 40;
-        int rowno = i % 20;
-        if (i % 20 == 0) {
-            output[i] = 'N';
+
+    for (int i = 0; i < 3200; i++) {
+        int rowindex = i / 80;
+        int colindex = i % 80;
+        int circmath = pow((colindex - 40),2) + pow((2*rowindex - 20),2);
+        int circmath2 = pow((colindex - 30),2) + pow((2*rowindex - 60),2);
+        output[79] = 'Y';
+       
+        output[81] = 'Y';
+        output[80 * 39 + 1] = 'Y';
+        output[3199] = 'Y';
+        if (circmath < 49 || circmath == 49) {
+            output[i] = 'B';
         }
-        else if (i > 400 && i < 440 || i > 360 && i < 400) {
-            output[i] = 'M';
+        if (2 * rowindex == colindex) {
+            output[i] = 'E';
+        }
+        if (circmath2 < 81 || circmath2 == 81) {
+            output[i] = 'G';
         }
     }
     printf("\x1b[2J");
-    for (int i = 0; i < 800; i++) {
-        if (i % 40 == 0) {
+    for (int i = 0; i < 3200; i++) {
+        if (i % 80 == 0) {
             cout << endl;
         }
         else {
